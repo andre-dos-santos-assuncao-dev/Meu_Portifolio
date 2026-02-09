7efa1af3950922e2ba24760b8fc96179a2848e52
André <andre.santos.assuncao.dev@gmail.com>
2026-02-09 14:00:04 -0300

    Adição da animação do botão 'verMais'

--- script.js
+++ script.js
@@ -1,13 +1,84 @@
 function verMais() {
-    let outrosProjetos = document.getElementById("outros_projetos");
+    let outrosProjetos = document.querySelectorAll(".outros_projetos");
     let botaoVerMais = document.querySelector(".botao_ver_mais");
+    let caixaProjetos = document.querySelector(".caixa_projetos");
 
-    if (outrosProjetos.style.display === "none") {
-        outrosProjetos.style.display = "block";
+    if (botaoVerMais.textContent === "Ver mais") {
+        caixaProjetos.animate([
+            { maxHeight: "360px" },
+            { maxHeight: "750px" }
+        ], {
+            duration: 500,
+            fill: "forwards"
+        });
+        for (let i = 0; i < outrosProjetos.length; i++) {
+            outrosProjetos[i].style.display = "block";
+            let h3 = outrosProjetos[i].querySelector("h3");
+            let button = outrosProjetos[i].querySelectorAll("button")
+            for (let i = 0; i < button.length; i++) {
+                button[i].animate([
+                    {padding: "0px 0px", fontSize: "0px"},
+                    {padding: "12px 12px", fontSize: "16px"}
+                ], {
+                    duration: 500,
+                    fill: "forwards"
+                });
+            }
+            h3.animate([
+                { fontSize: "0px"},
+                { fontSize: "20px"}
+            ], {
+                duration: 500,
+                fill: "forwards"
+            });
+            outrosProjetos[i].animate([
+                { maxHeight: "0px", transformOrigin: "top" },
+                { maxHeight: "300px" }
+            ], {
+                duration: 500,
+                fill: "forwards"
+            });
+        }
         botaoVerMais.textContent = "Ver menos";
     } else {
-
-        outrosProjetos.style.display = "none";
+        caixaProjetos.animate([
+            { maxHeight: "750px" },
+            { maxHeight: "360px" }
+        ], {
+            duration: 500,
+            fill: "forwards"
+        });
+        for (let i = 0; i < outrosProjetos.length; i++) {
+            let h3 = outrosProjetos[i].querySelector("h3");
+            let button = outrosProjetos[i].querySelectorAll("button");
+            for (let i = 0; i < button.length; i++) {
+                button[i].animate([
+                    {padding: "12px 12px", fontSize: "16px"},
+                    {padding: "0px 0px", fontSize: "0px"}
+                ], {
+                    duration: 400,
+                    fill: "forwards"
+                });
+            }
+            h3.animate([
+                { fontSize: "20px"},
+                { fontSize: "0px"}
+            ], {
+                duration: 500,
+                fill: "forwards"
+            });
+            outrosProjetos[i].animate([
+                { maxHeight: "300px"}, 
+                { maxHeight: "0px", transformOrigin: "top"  }
+            ], {    
+                duration: 500, 
+                fill: "forwards"
+            });
+            task = setTimeout(() => {       
+                outrosProjetos[i].style.display = "none";
+            }, 500);
+        }
+        
         botaoVerMais.textContent = "Ver mais";
     }
 }   
